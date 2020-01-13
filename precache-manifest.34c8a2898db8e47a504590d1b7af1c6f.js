acdaa7585532a0eb44a7be1e2cda65eb6185d670
Adam Bartniczak <adam.bartniczak@agora.pl>
2020-01-13 14:41:31 +0100

    initial commit

---
+++ precache-manifest.34c8a2898db8e47a504590d1b7af1c6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a7d2bf607fd195ae34a1c1e99965a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad308a2e22a1f719f23a",
+    "url": "/static/js/2.d919a949.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d919a949.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8608ae2030181273d5c",
+    "url": "/static/js/main.a36cd0ab.chunk.js"
+  },
+  {
+    "revision": "8457641d91866af12273",
+    "url": "/static/js/runtime-main.c9fa14c9.js"
+  }
+]);
